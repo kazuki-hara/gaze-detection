04385a5a662cdb52260613031dcdf51d00c40bc9
Kazuki-Hara <kazuki.h.21@gmail.com>
2021-07-18 00:54:41 +0900

    ウィンドウの縮小に対応

--- camera/capture.cpp
+++ camera/capture.cpp
@@ -1,9 +1,14 @@
+/* 
+雲台に搭載されたカメラから画像を取得
+*/
+
 #include <opencv2/opencv.hpp>
 #include "opencv2/highgui/highgui.hpp"
 
 cv::Mat frame;
 cv::Mat l_frame;
 cv::Mat r_frame;
+double ratio = 0.5;
 
 void camera_thread(void){
     cv::VideoCapture cap(0);
@@ -17,8 +22,9 @@ void camera_thread(void){
 		cv::Mat right_frame(frame, right);
 		cv::cvtColor(left_frame, l_frame, cv::COLOR_BGR2RGB);
 		cv::cvtColor(right_frame, r_frame, cv::COLOR_BGR2RGB);
+        cv::resize(left_frame, left_frame, cv::Size(), ratio, ratio);
         cv::imshow("video", left_frame);
         if(cv::waitKey(1) >= 0) break;
     }
-    
 }
+
