10f6702b417531752307bd7091af6ecab1bc71a8
Kazuki-Hara <kazuki.h.221@gmail.com>
2022-05-09 17:41:01 +0900

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 cd /share/home/hara/workspace/fove/src/build
 make
-./main control
-#cd /share/home/hara/workspace/fove/data/src
-#python3 calibration.py
-#cd /share/home/hara/workspace/fove/src/build
-#./main control
+./main 0
+cd /share/home/hara/workspace/fove/data/src
+python3 calibration.py
+cd /share/home/hara/workspace/fove/src/build
+./main 1
