e5c5fa81482dfacfa3dacf94b9c4ed542936ef4f
Kazuki-Hara <kazuki.h.221@gmail.com>
2022-05-09 17:40:20 +0900

    注視点の計算が合わないバグの修正

--- src/calibration/calibration.cpp
+++ src/calibration/calibration.cpp
@@ -18,6 +18,14 @@ std::vector<double> split(std::string s, char delim){
     return elems;
 }
 
+double pow(double x, int n){
+    double y = 1;
+    for(int i = 0; i < n; i++){
+        y = (double)y*x;
+    }
+    return y;
+}
+
 Calibration::Calibration(std::string param_file_path){
     std::ifstream file(param_file_path);
     std::string line;
@@ -25,10 +33,10 @@ Calibration::Calibration(std::string param_file_path){
         //std::cout << line << std::endl;
         parameter.push_back(split(line, ','));
     }
-    lx_pupil_center = parameter[5][0];
-    ly_pupil_center = parameter[5][1];
-    rx_pupil_center = parameter[5][2];
-    ry_pupil_center = parameter[5][3];
+    lx_pupil_center = parameter[4][0];
+    ly_pupil_center = parameter[4][1];
+    rx_pupil_center = parameter[4][2];
+    ry_pupil_center = parameter[4][3];
 }
 
 Calibration::~Calibration(){
@@ -41,15 +49,14 @@ std::tuple<double,double,double,double> Calibration::calcuration_gaze_point(doub
     ly_distance = ly - ly_pupil_center;
     rx_distance = rx - rx_pupil_center;
     ry_distance = ry - ry_pupil_center;
-    std::cout << "distance: " << lx_distance << " " << ly_distance << std::endl;
-    lx_disp = parameter[0][0] * std::pow(lx, 3.0) + parameter[0][1] * std::pow(lx, 2.0) * ly + parameter[0][2] * lx * std::pow(ly, 2.0) + parameter[0][3] * std::pow(ly, 3.0) + parameter[0][4] * std::pow(lx, 2.0) + parameter[0][5] * lx * ly + parameter[0][6] * std::pow(ly, 2.0) + parameter[0][7] * lx + parameter[0][8] * ly + parameter[0][9];
-    ly_disp = parameter[1][0] * std::pow(lx, 3.0) + parameter[1][1] * std::pow(lx, 2.0) * ly + parameter[1][2] * lx * std::pow(ly, 2.0) + parameter[1][3] * std::pow(ly, 3.0) + parameter[1][4] * std::pow(lx, 2.0) + parameter[1][5] * lx * ly + parameter[1][6] * std::pow(ly, 2.0) + parameter[1][7] * lx + parameter[1][8] * ly + parameter[1][9];
-    rx_disp = parameter[2][0] * std::pow(rx, 3.0) + parameter[2][1] * std::pow(rx, 2.0) * ry + parameter[2][2] * rx * std::pow(ry, 2.0) + parameter[2][3] * std::pow(ry, 3.0) + parameter[2][4] * std::pow(rx, 2.0) + parameter[2][5] * rx * ry + parameter[2][6] * std::pow(ry, 2.0) + parameter[2][7] * rx + parameter[2][8] * ry + parameter[2][9];
-    ry_disp = parameter[3][0] * std::pow(rx, 3.0) + parameter[3][1] * std::pow(rx, 2.0) * ry + parameter[3][2] * rx * std::pow(ry, 2.0) + parameter[3][3] * std::pow(ry, 3.0) + parameter[3][4] * std::pow(rx, 2.0) + parameter[3][5] * rx * ry + parameter[3][6] * std::pow(ry, 2.0) + parameter[3][7] * rx + parameter[3][8] * ry + parameter[3][9];
-    std::cout << "display: " << lx << " " << ly << std::endl;
+    lx_disp = parameter[0][0] * pow(lx_distance, 3) + parameter[0][1] * pow(lx_distance, 2) * ly_distance + parameter[0][2] * lx_distance * pow(ly_distance, 2) + parameter[0][3] * pow(ly_distance, 3) + parameter[0][4] * pow(lx_distance, 2) + parameter[0][5] * lx_distance * ly_distance + parameter[0][6] * pow(ly_distance, 2) + parameter[0][7] * lx_distance + parameter[0][8] * ly_distance + parameter[0][9];
+    ly_disp = parameter[1][0] * pow(lx_distance, 3) + parameter[1][1] * pow(lx_distance, 2) * ly_distance + parameter[1][2] * lx_distance * pow(ly_distance, 2) + parameter[1][3] * pow(ly_distance, 3) + parameter[1][4] * pow(lx_distance, 2) + parameter[1][5] * lx_distance * ly_distance + parameter[1][6] * pow(ly_distance, 2) + parameter[1][7] * lx_distance + parameter[1][8] * ly_distance + parameter[1][9];
+    rx_disp = parameter[2][0] * pow(rx_distance, 3) + parameter[2][1] * pow(rx_distance, 2) * ry_distance + parameter[2][2] * rx_distance * pow(ry_distance, 2) + parameter[2][3] * pow(ry_distance, 3) + parameter[2][4] * pow(rx_distance, 2) + parameter[2][5] * rx_distance * ry_distance + parameter[2][6] * pow(ry_distance, 2) + parameter[2][7] * rx_distance + parameter[2][8] * ry_distance + parameter[2][9];
+    ry_disp = parameter[3][0] * pow(rx_distance, 3) + parameter[3][1] * pow(rx_distance, 2) * ry_distance + parameter[3][2] * rx_distance * pow(ry_distance, 2) + parameter[3][3] * pow(ry_distance, 3) + parameter[3][4] * pow(rx_distance, 2) + parameter[3][5] * rx_distance * ry_distance + parameter[3][6] * pow(ry_distance, 2) + parameter[3][7] * rx_distance + parameter[3][8] * ry_distance + parameter[3][9];
     std::tuple<double, double, double, double> disp_gaze = std::make_tuple(lx_disp, ly_disp, rx_disp, ry_disp);
     return disp_gaze;
 }
+
 
 void Calibration::get_parameter(void){
     std::cout << lx_pupil_center << std::endl;
